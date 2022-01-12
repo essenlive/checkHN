0d713e46125ce12839381f5a3ec88cd2c40c76df
essenlive <perchais.quentin@gmail.com>
2022-01-12 12:32:39 +0100

    🐛 Cleanup and sanitization

--- popup/popup.js
+++ popup/popup.js
@@ -14,7 +14,7 @@ async function checkHN () {
   document.getElementById("url").innerText = `${currentUrl.host}${currentUrl.pathname}`
 
   // Fetch HN stories with the corresponding URL
-  let exactMatch = await fetch(`https://hn.algolia.com/api/v1/search?tags=story&query=${currentUrl.href}`)
+  let exactMatch = await fetch(`https://hn.algolia.com/api/v1/search?tags=story&query=${currentUrl.origin}${currentUrl.pathname}`)
   exactMatch = await exactMatch.json();
 
   // Hide the searching element
@@ -24,16 +24,26 @@ async function checkHN () {
   const noExactResult = document.getElementById("no-results");
   // Display the first five stories
   exactMatch.hits.slice(0,5).forEach(story => {
-    const storyElement = document.createElement('div');
-    storyElement.innerHTML = `<a href="https://news.ycombinator.com/item?id=${story.objectID}">${story.title}</a>   ${story.author} 🔺 ${story.points}`;
+    const storyElement = document.createElement('div');    
+
+    const link = document.createElement('a');
+    link.setAttribute('href', `https://news.ycombinator.com/item?id=${story.objectID}`);
+    link.innerText = story.title;
+    
+    const author = document.createElement('span');
+    author.innerText = `  ${story.author} 🔺 ${story.points} `;
+
+    storyElement.appendChild(link).insertAdjacentElement('afterend', author);
     noExactResult.insertAdjacentElement('beforebegin', storyElement);
   });
 
-  if (exactMatch.nbHits > 0 ) {
-    document.getElementById("matches").classList.add('results');
-  }
+  if (exactMatch.nbHits > 0 ) { document.getElementById("matches").classList.add('results'); }
   else{
-    noExactResult.innerHTML = `<span class="emoji">😭</span> No results. <a href="https://news.ycombinator.com/submitlink?u=${currentUrl.href}&t=${currentTitle}">Submit it ?</a> `
+    const link = document.createElement('a');
+    link.setAttribute('href', `https://news.ycombinator.com/submitlink?u=${currentUrl.href}&t=${currentTitle}`);
+    link.innerText = " Submit it ?";
+
+    noExactResult.appendChild(link)
     noExactResult.classList.remove('hidden')
   }
 
@@ -47,11 +57,24 @@ async function checkHN () {
   // If there are other stories on the hostname, or all the stories were not displayed, show a link to algolia.
   if (hostMatch?.nbHits > 0 || exactMatch.nbHits > 5) {
     const moreElement = document.createElement('div');
-    moreElement.classList.add('more-elements')
-    moreElement.innerHTML = `<span class="emoji">🎉</span> But there are other results for the same website : ${currentUrl.host}. <a href=https://hn.algolia.com/?tags=story&query=${currentUrl.host}>Wanna check them ?</a>`;
+    moreElement.setAttribute('class', 'more-elements');
+    
+    const emoji = document.createElement('span');
+    emoji.setAttribute('class', 'emoji');
+    emoji.innerText = "🎉"
+
+    const moreText = document.createElement('span');
+    moreText.innerText = ` But there are other results for the same website ${currentUrl.host} : `;
+
+    const link = document.createElement('a');
+    link.setAttribute('href', `https://hn.algolia.com/?tags=story&query=${currentUrl.host}` );
+    link.innerText = "Wanna check them ?";
+
+    
+    moreElement.appendChild(emoji).insertAdjacentElement('afterend', moreText).insertAdjacentElement('afterend',link);
+    
     document.getElementById("matches").insertAdjacentElement('afterend', moreElement);
   }
-  
 
 }
 
